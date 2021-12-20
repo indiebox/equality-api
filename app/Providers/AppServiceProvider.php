79e9b22cf8fc544877ad3b3f8edab2fcfcf50af9
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2021-12-20 15:47:55 +0300

    No need to specify the default string length for MySQL > 5.7.7

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 
@@ -25,8 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
-
         // Setup default password rule.
         Password::defaults(function() {
             $rule = Password::min(6);
