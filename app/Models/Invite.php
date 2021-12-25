91dc10761663906ebabb43324c5aedcc6596daf0
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2021-12-25 19:29:19 +0300

    Add sorting invites by status

--- app/Models/Invite.php
+++ app/Models/Invite.php
@@ -6,6 +6,30 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Invite
+ *
+ * @property int $id
+ * @property int $team_id
+ * @property int|null $inviter_id
+ * @property int $invited_id
+ * @property \Illuminate\Support\Carbon|null $accepted_at
+ * @property \Illuminate\Support\Carbon|null $declined_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User $invited
+ * @property-read \App\Models\User|null $inviter
+ * @property-read \App\Models\Team $team
+ * @method static \Database\Factories\InviteFactory factory(...$parameters)
+ * @method static Builder|Invite newModelQuery()
+ * @method static Builder|Invite newQuery()
+ * @method static Builder|Invite onlyAccepted()
+ * @method static Builder|Invite onlyDeclined()
+ * @method static Builder|Invite onlyPending()
+ * @method static Builder|Invite query()
+ * @method static Builder|Invite sortByStatus($status)
+ * @mixin \Eloquent
+ */
 class Invite extends Model
 {
     use HasFactory;
@@ -20,6 +44,17 @@ class Invite extends Model
     | Scopes
     |-------------------------------------------------------------
     */
+
+    public function scopeSortByStatus(Builder $query, $status) {
+        return $query
+            ->when($status == 'pending', function($query) {
+                return $query->onlyPending();
+            })->when($status == 'accepted', function($query) {
+                return $query->onlyAccepted();
+            })->when($status == 'declined', function($query) {
+                return $query->onlyDeclined();
+            });
+    }
 
     public function scopeOnlyPending(Builder $query) {
         return $query
