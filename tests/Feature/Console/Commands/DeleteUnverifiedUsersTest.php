b4501d5efb34b262539544cb62d99897e2b9d8df
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2021-12-27 17:32:08 +0300

    Add tests for deleting unverified users

---
+++ tests/Feature/Console/Commands/DeleteUnverifiedUsersTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature\Console\Commands;
+
+use App\Models\User;
+use Artisan;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class DeleteUnverifiedUsersTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function test_cant_delete_verified_users()
+    {
+        User::factory(5)->create(['updated_at' => now()->subDays(8)]);
+
+        Artisan::call('users:delete-unverified');
+
+        $this->assertDatabaseCount('users', 5);
+    }
+
+    public function test_cant_delete_unverified_users_before_week()
+    {
+        User::factory(5)->unverified()->create(['updated_at' => now()->subDays(6)]);
+
+        Artisan::call('users:delete-unverified');
+
+        $this->assertDatabaseCount('users', 5);
+    }
+
+    public function test_can_delete_unverified_users_after_week()
+    {
+        User::factory(5)->unverified()->create(['updated_at' => now()->subDays(8)]);
+
+        Artisan::call('users:delete-unverified');
+
+        $this->assertDatabaseCount('users', 0);
+    }
+}
