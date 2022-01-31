4c4390aad22c169211b989f0b687ab339d266bc7
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-01-31 12:22:48 +0300

    Change `email` key to `credentials` when throttle login attempt

--- app/Http/Requests/Api/V1/Auth/LoginUserRequest.php
+++ app/Http/Requests/Api/V1/Auth/LoginUserRequest.php
@@ -59,7 +59,7 @@ class LoginUserRequest extends FormRequest
         $seconds = RateLimiter::availableIn($this->throttleKey());
 
         throw ValidationException::withMessages([
-            'email' => trans('auth.throttle', [
+            'credentials' => trans('auth.throttle', [
                 'seconds' => $seconds,
                 'minutes' => ceil($seconds / 60),
             ]),
