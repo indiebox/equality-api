9d9274193b9743cbd312fca5774321bb59c3370a
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-02-02 11:41:09 +0300

    Change messages style

--- resources/views/auth/registered.blade.php
+++ resources/views/auth/registered.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.html')
 
 @section('content')
-    <div class="flex flex-col justify-center w-1/2 p-8 m-auto bg-green-400">
+    <div class="flex flex-col justify-center items-center h-[100vh]">
         <p>Регистрация прошла успешна.</p>
         <p>На указанную вами почту было выслано письмо с ссылкой для подтверждения аккаунта.</p>
         <p>Подтвердите ваш аккаунт, чтобы начать использовать {{ config('app.name') }}.</p>
