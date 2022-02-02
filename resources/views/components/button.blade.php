be53d1a753222a58da5acce24bbc49bc402cffc6
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-02-02 17:53:13 +0300

    Add touch effect to button

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -2,6 +2,8 @@
 @slot default Button text.
 --}}
 
-<button {{ $attributes->merge(['class' => 'mdc-button mdc-button--raised']) }}>
+<button {{ $attributes->merge(['class' => 'mdc-button mdc-button--raised mdc-button--touch']) }}>
+    <span class="mdc-button__ripple"></span>
+    <span class="mdc-button__touch"></span>
     <span class="mdc-button__label">{{ $slot }}</span>
 </button>
