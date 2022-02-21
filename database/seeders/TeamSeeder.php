cb9df7ceaf6fb41c33b87dee1c1026b12eda1acf
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-02-21 14:23:38 +0300

    Improve seader

--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -18,12 +18,19 @@ class TeamSeeder extends Seeder
     {
         $teams = Team::factory(3)->has(User::factory(3), 'members')->create();
 
-        // Set team creators.
+        // Set the team creators.
         $teams[0]->members()->attach($user1 = User::where('email', 'admin1@mail.ru')->first(), ['is_creator' => true]);
         $teams[1]->members()->attach($user2 = User::where('email', 'admin2@mail.ru')->first(), ['is_creator' => true]);
         $teams[2]->members()->attach($user3 = User::factory()->create(), ['is_creator' => true]);
 
-        // Create invites(user1 and user2 will have 2 invites).
+        // Creating additional teams for main users.
+        $user1->teams()->saveMany(Team::factory()->make());
+        $user2->teams()->saveMany(Team::factory()->make());
+
+        // Creating common team for main users.
+        Team::factory()->hasAttached([$user1, $user2])->create();
+
+        // Creating invites(user1 and user2 will have 2 invites).
         Invite::factory()->team($teams[0])->inviter($user1)->invited($user2)->create();
         Invite::factory()->team($teams[1])->inviter($user2)->invited($user1)->create();
         Invite::factory()->team($teams[2])->inviter($user3)->invited($user1)->create();
