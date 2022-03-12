1fc88d583bb68adacf14f02b64c80f18121867b6
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-03-12 11:36:06 +0300

    Add resources

---
+++ app/Http/Resources/V1/Board/BoardResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\V1\Board;
+
+use App\Http\Resources\V1\Project\ProjectResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BoardResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'project' => new ProjectResource($this->project),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
