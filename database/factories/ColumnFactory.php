729a28dc8c72e84f2658769a4159deffd89a5c07
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-03-13 12:10:23 +0300

    Add database specified stuff

---
+++ database/factories/ColumnFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ColumnFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->words(2, true),
+        ];
+    }
+
+    /** Setup board for the column.
+     * @param \Illuminate\Database\Eloquent\Factories\Factory|\Illuminate\Database\Eloquent\Model $board
+     * @return static
+     */
+    public function board($board)
+    {
+        return $this->for($board);
+    }
+}
