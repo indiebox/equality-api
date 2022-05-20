352146001127bcddd49ed380b84ba5a10c664b0b
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-05-20 11:17:54 +0300

    Add test for optional columns

--- tests/Feature/Services/Boards/ModuleServiceTest.php
+++ tests/Feature/Services/Boards/ModuleServiceTest.php
@@ -28,7 +28,7 @@ class ModuleServiceTest extends TestCase
         $this->service = app(ModuleService::class);
     }
 
-    public function test_enable_kanban_method_1()
+    public function test_enable_kanban_method_with_single_column()
     {
         $board = Board::factory()->project(Project::factory()->team(Team::factory()))->create();
         $columns = Column::factory()->board($board)->create();
@@ -64,7 +64,7 @@ class ModuleServiceTest extends TestCase
         $this->assertEquals(ColumnType::TODO, $columns[0]->column_type_id);
         $this->assertEquals(ColumnType::NONE, $columns[1]->column_type_id);
     }
-    public function test_enable_kanban_method_2()
+    public function test_enable_kanban_method_without_optional_columns()
     {
         $board = Board::factory()->project(Project::factory()->team(Team::factory()))->create();
         $columns = Column::factory(4)->board($board)->create();
@@ -94,7 +94,7 @@ class ModuleServiceTest extends TestCase
         $this->assertEquals(ColumnType::IN_PROGRESS, $columns[4]->column_type_id);
         $this->assertEquals('In Progress', $columns[4]->name);
     }
-    public function test_enable_kanban_method_3()
+    public function test_enable_kanban_method_with_optional_columns()
     {
         $board = Board::factory()->project(Project::factory()->team(Team::factory()))->create();
         $columns = Column::factory(5)->board($board)->create();
@@ -126,6 +126,21 @@ class ModuleServiceTest extends TestCase
         $this->assertEquals(ColumnType::IN_PROGRESS, $columns[5]->column_type_id);
         $this->assertEquals('In Progress', $columns[5]->name);
         $this->assertEquals(ColumnType::ON_REVIEW, $columns[6]->column_type_id);
+        $this->assertEquals('On Review', $columns[6]->name);
+
+        // Dont pass optional columns, so columns with that types should unset.
+        $this->service->enableKanban($board, [
+            'todo_column_id' => $columns[0],
+            'inprogress_column_id' => $columns[5],
+            'done_column_id' => $columns[2],
+        ]);
+
+        $this->assertDatabaseCount('board_module', 1);
+        $columns = Column::all();
+        $this->assertEquals(3, $columns->filter(fn($column) => $column->column_type_id != ColumnType::NONE)->count());
+        $this->assertEquals(ColumnType::IN_PROGRESS, $columns[5]->column_type_id);
+        $this->assertEquals('In Progress', $columns[5]->name);
+        $this->assertEquals(ColumnType::NONE, $columns[6]->column_type_id);
         $this->assertEquals('On Review', $columns[6]->name);
     }
 
